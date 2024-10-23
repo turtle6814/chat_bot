9be5f2bcd5e218db668e1a13830af69c3297654a
QuocAnVuong <vuongquocan2308@gmail.com>
2024-10-23 14:33:25 +0700

    Change from ServiceContext to Settings

--- app.py
+++ app.py
@@ -1,23 +1,61 @@
+import os
+import openai
 import chainlit as cl
 
-@cl.set_starters
-async def set_starters():
-    return [
-        cl.Starter(
-            label="Morning routine ideation",
-            message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
-            ),
+from llama_index.core import (
+    Settings,
+    StorageContext,
+    VectorStoreIndex,
+    SimpleDirectoryReader,
+    load_index_from_storage,
+)
+from llama_index.llms.openai import OpenAI
+from llama_index.embeddings.openai import OpenAIEmbedding
+from llama_index.core.query_engine.retriever_query_engine import RetrieverQueryEngine
+from llama_index.core.callbacks import CallbackManager
+from llama_index.core import Settings
 
-        cl.Starter(
-            label="Explain superconductors",
-            message="Explain superconductors like I'm five years old.",
-            ),
-        cl.Starter(
-            label="Python script for daily email reports",
-            message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
-            ),
-        cl.Starter(
-            label="Text inviting friend to wedding",
-            message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
-            )
-        ]
+openai.api_key = "Your API"
+
+try:
+    # rebuild storage context
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    # load index
+    index = load_index_from_storage(storage_context)
+except:
+    documents = SimpleDirectoryReader("./data").load_data(show_progress=True)
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist()
+
+
+@cl.on_chat_start
+async def start():
+    Settings.llm = OpenAI(
+        model="gpt-3.5-turbo", temperature=0.1, max_tokens=1024, streaming=True
+    )
+    Settings.embed_model = OpenAIEmbedding(model="text-embedding-3-small")
+    Settings.context_window = 4096
+
+    Settings.callback_manager = CallbackManager(
+        [cl.LlamaIndexCallbackHandler()])
+    query_engine = index.as_query_engine(
+        streaming=True, similarity_top_k=2)
+    cl.user_session.set("query_engine", query_engine)
+
+    await cl.Message(
+        author="Assistant", content="Hello! Im an AI assistant. How may I help you?"
+    ).send()
+
+
+@cl.on_message
+async def main(message: cl.Message):
+    query_engine = cl.user_session.get(
+        "query_engine")  # type: RetrieverQueryEngine
+
+    msg = cl.Message(content="", author="Assistant")
+
+    res = await cl.make_async(query_engine.query)(message.content)
+
+    for token in res.response_gen:
+        await msg.stream_token(token)
+    await msg.send()
